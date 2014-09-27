cf2bc6e7f4d06db7f7b677d2a620dd6b8572033d
Nicolas Widart <n.widart@gmail.com>
2014-09-27 18:56:15 +0200

    First Commit

---
+++ Http/Controllers/BlogController.php
@@ -0,0 +1,8 @@
+<?php namespace Modules\Blog\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+
+class BlogController extends Controller
+{
+
+}
---
+++ Http/routes.php
@@ -0,0 +1,8 @@
+<?php
+
+Route::group(
+    ['prefix' => 'blog', 'namespace' => 'Modules\Blog\Http\Controllers'],
+    function () {
+        Route::get('/', 'BlogController@index');
+    }
+);
