162f0eab2629d68db31202b59d0d4733c95650c1
Nicolas Widart <n.widart@gmail.com>
2014-09-28 08:19:14 +0200

    Adding the permissions

---
+++ Config/permissions.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'posts' => [
+        'index',
+        'create',
+        'store',
+        'edit',
+        'update',
+        'destroy'
+    ],
+    'categories' => [
+        'index',
+        'create',
+        'store',
+        'edit',
+        'update',
+        'destroy'
+    ],
+    'tags' => [
+        'index',
+        'create',
+        'store',
+        'edit',
+        'update',
+        'destroy'
+    ],
+];
