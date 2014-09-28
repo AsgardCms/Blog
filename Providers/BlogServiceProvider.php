a6dceac959770bfc20173c72b02a9e1984cde268
Nicolas Widart <n.widart@gmail.com>
2014-09-28 08:24:37 +0200

    Adding the permission filter

--- Providers/BlogServiceProvider.php
+++ Providers/BlogServiceProvider.php
@@ -1,10 +1,10 @@
 <?php namespace Modules\Blog\Providers;
 
+use Illuminate\Routing\Router;
 use Illuminate\Support\ServiceProvider;
 
 class BlogServiceProvider extends ServiceProvider
 {
-
     /**
      * Indicates if loading of the provider is deferred.
      *
@@ -13,13 +13,39 @@ class BlogServiceProvider extends ServiceProvider
     protected $defer = false;
 
     /**
+     * The filters base class name.
+     *
+     * @var array
+     */
+    protected $filters = [
+        'permissions' => 'PermissionFilter'
+    ];
+
+    /**
      * Register the service provider.
      *
      * @return void
      */
     public function register()
     {
-        //
+        $this->app->booted(function ($app) {
+            $this->registerFilters($app['router']);
+        });
+    }
+
+    /**
+     * Register the filters.
+     *
+     * @param  Router $router
+     * @return void
+     */
+    public function registerFilters(Router $router)
+    {
+        foreach ($this->filters as $name => $filter) {
+            $class = 'Modules\\Blog\\Http\\Filters\\' . $filter;
+
+            $router->filter($name, $class);
+        }
     }
 
     /**
