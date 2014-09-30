6b4158c94103c7e16598b1b56bcd0f40f685a4f5
Nicolas Widart <n.widart@gmail.com>
2014-09-30 13:23:49 +0200

    Adding basic repositories methods stubs

--- Repositories/Eloquent/EloquentTagRepository.php
+++ Repositories/Eloquent/EloquentTagRepository.php
@@ -4,5 +4,43 @@ use Modules\Blog\Repositories\TagRepository;
 
 class EloquentTagRepository implements TagRepository
 {
+    /**
+     * Create a tag
+     * @param $data
+     * @return mixed
+     */
+    public function create($data)
+    {
+        // TODO: Implement create() method.
+    }
 
+    /**
+     * Find a tag by id
+     * @param $id
+     * @return mixed
+     */
+    public function find($id)
+    {
+        // TODO: Implement find() method.
+    }
+
+    /**
+     * Find all tags
+     * @return mixed
+     */
+    public function all()
+    {
+        // TODO: Implement all() method.
+    }
+
+    /**
+     * Update a tag by its ID
+     * @param $id
+     * @param $data
+     * @return mixed
+     */
+    public function update($id, $data)
+    {
+        // TODO: Implement update() method.
+    }
 }
--- Repositories/TagRepository.php
+++ Repositories/TagRepository.php
@@ -1,6 +1,36 @@
 <?php namespace Modules\Blog\Repositories;
 
+/**
+ * Interface TagRepository
+ * @package Modules\Blog\Repositories
+ */
 interface TagRepository
 {
+    /**
+     * Create a tag
+     * @param $data
+     * @return mixed
+     */
+    public function create($data);
 
+    /**
+     * Find a tag by id
+     * @param $id
+     * @return mixed
+     */
+    public function find($id);
+
+    /**
+     * Find all tags
+     * @return mixed
+     */
+    public function all();
+
+    /**
+     * Update a tag by its ID
+     * @param $id
+     * @param $data
+     * @return mixed
+     */
+    public function update($id, $data);
 }
