01af607ac3a0d4e7966029185e3d338e83cd7ec3
Nicolas Widart <n.widart@gmail.com>
2014-10-03 09:31:59 +0200

    Adding route i18n and filter

--- Composers/SidebarViewComposer.php
+++ Composers/SidebarViewComposer.php
@@ -10,19 +10,19 @@ class SidebarViewComposer
         $view->items->put('blog', Collection::make([
             [
                 'weight' => '3',
-                'request' => Request::is("{$view->prefix}/posts*") or Request::is("{$view->prefix}/categories*"),
+                'request' => Request::is("*/{$view->prefix}/posts*") or Request::is("*/{$view->prefix}/categories*"),
                 'route' => '#',
                 'icon-class' => 'fa fa-copy',
                 'title' => 'Blog',
             ],
             [
-                'request' => "{$view->prefix}/posts*",
+                'request' => "*/{$view->prefix}/posts*",
                 'route' => 'dashboard.post.index',
                 'icon-class' => 'fa fa-file-text',
                 'title' => 'Posts',
             ],
             [
-                'request' => "{$view->prefix}/categories*",
+                'request' => "*/{$view->prefix}/categories*",
                 'route' => 'dashboard.category.index',
                 'icon-class' => 'fa fa-file-text',
                 'title' => 'Categories',
--- Http/routes.php
+++ Http/routes.php
@@ -1,25 +1,28 @@
 <?php
-Route::group([
-    'prefix' => Config::get('core::core.admin-prefix'),
-    'namespace' => 'Modules\Blog\Http\Controllers'], function () {
+Route::group(['prefix' => LaravelLocalization::setLocale(), 'before' => 'LaravelLocalizationRedirectFilter'], function()
+{
+    Route::group([
+        'prefix' => Config::get('core::core.admin-prefix'),
+        'namespace' => 'Modules\Blog\Http\Controllers'], function () {
 
-    Route::resource('posts', 'Admin\PostController', ['except' => ['show'], 'names' => [
-        'index' => 'dashboard.post.index',
-        'create' => 'dashboard.post.create',
-        'store' => 'dashboard.post.store',
-        'edit' => 'dashboard.post.edit',
-        'update' => 'dashboard.post.update',
-        'destroy' => 'dashboard.post.destroy',
-    ]]);
+        Route::resource('posts', 'Admin\PostController', ['except' => ['show'], 'names' => [
+            'index' => 'dashboard.post.index',
+            'create' => 'dashboard.post.create',
+            'store' => 'dashboard.post.store',
+            'edit' => 'dashboard.post.edit',
+            'update' => 'dashboard.post.update',
+            'destroy' => 'dashboard.post.destroy',
+        ]]);
 
-    Route::resource('categories', 'Admin\CategoryController', ['except' => ['show'], 'names' => [
-        'index' => 'dashboard.category.index',
-        'create' => 'dashboard.category.create',
-        'store' => 'dashboard.category.store',
-        'edit' => 'dashboard.category.edit',
-        'update' => 'dashboard.category.update',
-        'destroy' => 'dashboard.category.destroy',
-    ]]);
+        Route::resource('categories', 'Admin\CategoryController', ['except' => ['show'], 'names' => [
+            'index' => 'dashboard.category.index',
+            'create' => 'dashboard.category.create',
+            'store' => 'dashboard.category.store',
+            'edit' => 'dashboard.category.edit',
+            'update' => 'dashboard.category.update',
+            'destroy' => 'dashboard.category.destroy',
+        ]]);
+    });
 });
 
 Route::group([
