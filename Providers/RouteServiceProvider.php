f4960e279459ce0ef2aa1416eacb711b6629d7ae
Nicolas Widart <n.widart@gmail.com>
2014-10-17 13:03:43 +0200

    Adapting the new route controller

--- Providers/RouteServiceProvider.php
+++ Providers/RouteServiceProvider.php
@@ -1,23 +1,37 @@
 <?php namespace Modules\Blog\Providers;
 
 use Illuminate\Routing\Router;
-use Illuminate\Contracts\Routing\UrlGenerator;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 
 class RouteServiceProvider extends ServiceProvider
 {
     /**
+     * The root namespace to assume when generating URLs to actions.
+     *
+     * @var string
+     */
+    protected $rootUrlNamespace = 'Modules\Blog\Http\Controllers';
+
+    /**
+     * The controllers to scan for route annotations.
+     *
+     * @var array
+     */
+    protected $scan = [
+        'Modules\Blog\Http\Controllers',
+    ];
+
+    /**
      * Called before routes are registered.
      *
      * Register any model bindings or pattern based filters.
      *
-     * @param  Router  $router
-     * @param  UrlGenerator  $url
+     * @param  Router $router
      * @return void
      */
-    public function before(Router $router, UrlGenerator $url)
+    public function before(Router $router)
     {
-        $url->setRootControllerNamespace('Modules\Blog\Http\Controllers');
+        //
     }
 
     /**
@@ -25,17 +39,10 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function map()
+    public function map(Router $router)
     {
-        // Once the application has booted, we will include the default routes
-        // file. This "namespace" helper will load the routes file within a
-        // route group which automatically sets the controller namespace.
-        $this->app->booted(function()
-        {
-            $this->namespaced('Modules\Blog\Http\Controllers', function(Router $router)
-            {
-                require __DIR__ . '/../Http/routes.php';
-            });
-        });
+        require __DIR__ . '/../Http/routes.php';
     }
+
 }
+
