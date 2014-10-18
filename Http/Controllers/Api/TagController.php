4e898677bb9f438313a322b3978df077a4872ff5
Nicolas Widart <n.widart@gmail.com>
2014-10-18 09:46:13 +0200

    Removing unused code

--- Http/Controllers/Api/TagController.php
+++ Http/Controllers/Api/TagController.php
@@ -60,37 +60,4 @@ class TagController
     {
         return Response::json($this->tag->findByName($name));
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int $id
-     * @return Response
-     */
-    public function edit($id)
-    {
-        return \View::make('collection.edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  int $id
-     * @return Response
-     */
-    public function update($id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
