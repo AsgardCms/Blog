a6bba2ca3ce2cb63d0989f312a83448543e6963e
Nicolas Widart <n.widart@gmail.com>
2014-10-21 13:38:25 +0200

    Adding fictive setting options

--- Config/settings.php
+++ Config/settings.php
@@ -1,5 +1,16 @@
 <?php
 
 return [
-    'posts-per-page' => trans('blog::settings.posts-per-page'),
+//    'posts-per-page' => [
+//        'description' => trans('blog::settings.posts-per-page'),
+//        'type' => 'text'
+//    ],
+    'this-is-a-checkbox' => [
+        'description' => 'This is a checkbox',
+        'type' => 'checkbox'
+    ],
+//    'this-is-a-textarea' => [
+//        'description' => 'This is a textarea',
+//        'type' => 'textarea'
+//    ],
 ];
