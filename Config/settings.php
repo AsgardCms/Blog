3f86dee12af602b665e89630cefe14ea033a537d
Nicolas Widart <n.widart@gmail.com>
2014-10-21 20:50:29 +0200

    Activating settings

--- Config/settings.php
+++ Config/settings.php
@@ -1,16 +1,16 @@
 <?php
 
 return [
-//    'posts-per-page' => [
-//        'description' => trans('blog::settings.posts-per-page'),
-//        'type' => 'text'
-//    ],
+    'posts-per-page' => [
+        'description' => trans('blog::settings.posts-per-page'),
+        'view' => 'setting::admin.partials.module-text-field'
+    ],
     'this-is-a-checkbox' => [
         'description' => 'This is a checkbox',
-        'type' => 'checkbox'
+        'view' => 'setting::admin.partials.module-checkbox-field'
     ],
-//    'this-is-a-textarea' => [
-//        'description' => 'This is a textarea',
-//        'type' => 'textarea'
-//    ],
+    'this-is-a-textarea' => [
+        'description' => 'This is a textarea',
+        'view' => 'setting::admin.partials.module-textarea-field'
+    ],
 ];
