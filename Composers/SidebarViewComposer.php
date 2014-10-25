a3a469cd8a994f4068b14848d95ccdea4785c474
Nicolas Widart <n.widart@gmail.com>
2014-10-25 22:41:34 +0200

    Adding permission key

--- Composers/SidebarViewComposer.php
+++ Composers/SidebarViewComposer.php
@@ -3,8 +3,9 @@
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Request;
 use Illuminate\View\View;
+use Modules\Core\Composers\BaseSidebarViewComposer;
 
-class SidebarViewComposer
+class SidebarViewComposer extends BaseSidebarViewComposer
 {
     public function compose(View $view)
     {
@@ -15,18 +16,21 @@ class SidebarViewComposer
                 'route' => '#',
                 'icon-class' => 'fa fa-copy',
                 'title' => 'Blog',
+                'permission' => $this->auth->hasAccess('posts.index') or $this->auth->hasAccess('categories.index')
             ],
             [
                 'request' => "*/{$view->prefix}/posts*",
                 'route' => 'dashboard.post.index',
                 'icon-class' => 'fa fa-file-text',
                 'title' => 'Posts',
+                'permission' => $this->auth->hasAccess('posts.index')
             ],
             [
                 'request' => "*/{$view->prefix}/categories*",
                 'route' => 'dashboard.category.index',
                 'icon-class' => 'fa fa-file-text',
                 'title' => 'Categories',
+                'permission' => $this->auth->hasAccess('categories.index')
             ]
         ]));
     }
--- Config/settings.php
+++ Config/settings.php
@@ -3,27 +3,33 @@
 return [
     'posts-per-page' => [
         'description' => trans('blog::settings.posts-per-page'),
-        'view' => 'setting::admin.partials.module-text-field'
+        'view' => 'text',
+        'translatable' => true
     ],
     'this-is-a-checkbox' => [
         'description' => 'This is a checkbox',
-        'view' => 'setting::admin.partials.module-checkbox-field'
+        'view' => 'checkbox',
+        'translatable' => true
     ],
-    'this-is-a-textarea' => [
-        'description' => 'This is a textarea',
-        'view' => 'setting::admin.partials.module-textarea-field'
+    'plain-non-translated-setting' => [
+        'description' => 'A plain setting',
+        'view' => 'text',
     ],
-    'this-is-a-radio' => [
-        'description' => 'This is a radio',
-        'options' => [
-            'option1' => 'Option 1',
-            'option2' => 'Option 2',
-            'option3' => 'Option 3',
-        ],
-        'view' => 'setting::admin.partials.module-radio-field'
-    ],
-    'this-is-a-number-info' => [
-        'description' => 'This is a number input',
-        'view' => 'setting::admin.partials.module-number-field'
-    ],
+//    'this-is-a-textarea' => [
+//        'description' => 'This is a textarea',
+//        'view' => 'setting::admin.fields.translatable.textarea'
+//    ],
+//    'this-is-a-radio' => [
+//        'description' => 'This is a radio',
+//        'options' => [
+//            'option1' => 'Option 1',
+//            'option2' => 'Option 2',
+//            'option3' => 'Option 3',
+//        ],
+//        'view' => 'setting::admin.fields.translatable.radio'
+//    ],
+//    'this-is-a-number-info' => [
+//        'description' => 'This is a number input',
+//        'view' => 'setting::admin.fields.translatable.number'
+//    ],
 ];
