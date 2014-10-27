28133c58011d06ba45fb767ee6881a94d801ee6d
Nicolas Widart <n.widart@gmail.com>
2014-10-27 16:32:57 +0100

    Adding slugify option to posts

--- Resources/views/admin/posts/partials/create-fields.blade.php
+++ Resources/views/admin/posts/partials/create-fields.blade.php
@@ -1,12 +1,12 @@
 <div class="box-body">
     <div class='form-group{{ $errors->has("title[{$lang}]") ? ' has-error' : '' }}'>
         {!! Form::label("title[{$lang}]", trans('blog::post.form.title')) !!}
-        {!! Form::text("title[{$lang}]", Input::old("title[{$lang}]"), ['class' => 'form-control', 'placeholder' => trans('blog::post.form.title')]) !!}
+        {!! Form::text("title[{$lang}]", Input::old("title[{$lang}]"), ['class' => 'form-control slugify', 'placeholder' => trans('blog::post.form.title')]) !!}
         {!! $errors->first("title[{$lang}]", '<span class="help-block">:message</span>') !!}
     </div>
     <div class='form-group{{ $errors->has("slug[{$lang}]") ? ' has-error' : '' }}'>
        {!! Form::label("slug[{$lang}]", trans('blog::post.form.slug')) !!}
-       {!! Form::text("slug[{$lang}]", Input::old("slug[{$lang}]"), ['class' => 'form-control', 'placeholder' => trans('blog::post.form.slug')]) !!}
+       {!! Form::text("slug[{$lang}]", Input::old("slug[{$lang}]"), ['class' => 'form-control slug', 'placeholder' => trans('blog::post.form.slug')]) !!}
        {!! $errors->first("slug[{$lang}]", '<span class="help-block">:message</span>') !!}
     </div>
 
--- Resources/views/admin/posts/partials/edit-fields.blade.php
+++ Resources/views/admin/posts/partials/edit-fields.blade.php
@@ -1,12 +1,12 @@
 <div class="box-body">
     <div class='form-group{{ $errors->has("title[{$lang}]") ? ' has-error' : '' }}'>
         {!! Form::label("title[{$lang}]", trans('blog::post.form.title')) !!}
-        {!! Form::text("title[{$lang}]", Input::old("title[{$lang}]", $post->translate($lang)->title), ['class' => 'form-control', 'placeholder' => trans('blog::post.form.title')]) !!}
+        {!! Form::text("title[{$lang}]", Input::old("title[{$lang}]", $post->translate($lang)->title), ['class' => 'form-control slugify', 'placeholder' => trans('blog::post.form.title')]) !!}
         {!! $errors->first("title[{$lang}]", '<span class="help-block">:message</span>') !!}
     </div>
     <div class='form-group{{ $errors->has("slug[{$lang}]") ? ' has-error' : '' }}'>
        {!! Form::label("slug[{$lang}]", trans('blog::post.form.slug')) !!}
-       {!! Form::text("slug[{$lang}]", Input::old("slug[{$lang}]", $post->translate($lang)->slug), ['class' => 'form-control', 'placeholder' => trans('blog::post.form.slug')]) !!}
+       {!! Form::text("slug[{$lang}]", Input::old("slug[{$lang}]", $post->translate($lang)->slug), ['class' => 'form-control slug', 'placeholder' => trans('blog::post.form.slug')]) !!}
        {!! $errors->first("slug[{$lang}]", '<span class="help-block">:message</span>') !!}
     </div>
 
