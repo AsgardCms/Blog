7b0845785349e309460a404530aab1c354eed9d4
Nicolas Widart <n.widart@gmail.com>
2014-10-27 16:35:23 +0100

    Adding slugify option to categories

--- Resources/views/admin/categories/partials/edit-fields.blade.php
+++ Resources/views/admin/categories/partials/edit-fields.blade.php
@@ -1,12 +1,12 @@
 <div class="box-body">
     <div class='form-group{{ $errors->has("name[{$lang}]") ? ' has-error' : '' }}'>
         {!! Form::label("name[{$lang}]", trans('blog::category.form.name')) !!}
-        {!! Form::text("name[{$lang}]", Input::old("name[{$lang}]", $category->translate($lang)->name), ['class' => 'form-control', 'placeholder' => trans('blog::category.form.name')]) !!}
+        {!! Form::text("name[{$lang}]", Input::old("name[{$lang}]", $category->translate($lang)->name), ['class' => 'form-control slugify', 'placeholder' => trans('blog::category.form.name')]) !!}
         {!! $errors->first("name[{$lang}]", '<span class="help-block">:message</span>') !!}
     </div>
     <div class='form-group{{ $errors->has("slug[{$lang}]") ? ' has-error' : '' }}'>
            {!! Form::label("slug[{$lang}]", trans('blog::category.form.slug')) !!}
-           {!! Form::text("slug[{$lang}]", Input::old("slug[{$lang}]", $category->translate($lang)->slug), ['class' => 'form-control', 'placeholder' => trans('blog::category.form.slug')]) !!}
+           {!! Form::text("slug[{$lang}]", Input::old("slug[{$lang}]", $category->translate($lang)->slug), ['class' => 'form-control slug', 'placeholder' => trans('blog::category.form.slug')]) !!}
            {!! $errors->first("slug[{$lang}]", '<span class="help-block">:message</span>') !!}
        </div>
 </div>
