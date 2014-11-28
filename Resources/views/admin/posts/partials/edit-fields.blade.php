9d5d8638554e932b88addcb724e3778b38c1d61b
Nicolas Widart <n.widart@gmail.com>
2014-11-28 16:56:26 +0100

    Check for presence of property

--- Resources/views/admin/posts/partials/edit-fields.blade.php
+++ Resources/views/admin/posts/partials/edit-fields.blade.php
@@ -1,18 +1,21 @@
 <div class="box-body">
     <div class='form-group{{ $errors->has("{$lang}[title]") ? ' has-error' : '' }}'>
+        <?php $oldTitle = isset($post->translate($lang)->title) ? $post->translate($lang)->title : ''; ?>
         {!! Form::label("{$lang}[title]", trans('blog::post.form.title')) !!}
-        {!! Form::text("{$lang}[title]", Input::old("{$lang}[title]", $post->translate($lang)->title), ['class' => 'form-control slugify', 'placeholder' => trans('blog::post.form.title')]) !!}
+        {!! Form::text("{$lang}[title]", Input::old("{$lang}[title]", $oldTitle), ['class' => 'form-control slugify', 'placeholder' => trans('blog::post.form.title')]) !!}
         {!! $errors->first("{$lang}[title]", '<span class="help-block">:message</span>') !!}
     </div>
     <div class='form-group{{ $errors->has("{$lang}[slug]") ? ' has-error' : '' }}'>
+        <?php $oldSlug = isset($post->translate($lang)->slug) ? $post->translate($lang)->slug : ''; ?>
        {!! Form::label("{$lang}[slug]", trans('blog::post.form.slug')) !!}
-       {!! Form::text("{$lang}[slug]", Input::old("{$lang}[slug]", $post->translate($lang)->slug), ['class' => 'form-control slug', 'placeholder' => trans('blog::post.form.slug')]) !!}
+       {!! Form::text("{$lang}[slug]", Input::old("{$lang}[slug]", $oldSlug), ['class' => 'form-control slug', 'placeholder' => trans('blog::post.form.slug')]) !!}
        {!! $errors->first("{$lang}[slug]", '<span class="help-block">:message</span>') !!}
     </div>
 
     <div class='box-body pad'>
+        <?php $oldContent = isset($post->translate($lang)->content) ? $post->translate($lang)->content : ''; ?>
         <textarea class="ckeditor" name="{{$lang}}[content]" rows="10" cols="80">
-        {!! $post->translate($lang)->content !!}
+        {!! $oldContent !!}
         </textarea>
     </div>
 </div>
