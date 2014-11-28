d38f49520dfd4ec1c9ecf12463b461354bb2b7a2
Nicolas Widart <n.widart@gmail.com>
2014-11-28 16:46:52 +0100

    Get the posts for index page

--- Http/Controllers/PublicController.php
+++ Http/Controllers/PublicController.php
@@ -1,12 +1,26 @@
 <?php namespace Modules\Blog\Http\Controllers;
 
+use Modules\Blog\Repositories\PostRepository;
 use Modules\Core\Http\Controllers\BasePublicController;
 
 class PublicController extends BasePublicController
 {
+    /**
+     * @var PostRepository
+     */
+    private $post;
+
+    public function __construct(PostRepository $post)
+    {
+        parent::__construct();
+        $this->post = $post;
+    }
+
     public function index()
     {
-        return view('index');
+        $posts = $this->post->all();
+
+        return view('blog.index', compact('posts'));
     }
 
     public function show($slug)
