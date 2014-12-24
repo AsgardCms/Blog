71dd9e5f5c541e0c8e3b8506d80d088cb16423cb
Nicolas Widart <n.widart@gmail.com>
2014-12-24 20:55:05 +0100

    Remove unneeded methods from concrete class

--- Repositories/Eloquent/EloquentCategoryRepository.php
+++ Repositories/Eloquent/EloquentCategoryRepository.php
@@ -7,29 +7,6 @@ use Modules\Core\Repositories\Eloquent\EloquentBaseRepository;
 class EloquentCategoryRepository extends EloquentBaseRepository implements CategoryRepository
 {
     /**
-     * Create a category
-     * @param $data
-     * @return mixed
-     */
-    public function create($data)
-    {
-        $this->model->create($data);
-    }
-
-    /**
-     * Update a category
-     * @param Category $category
-     * @param array $data
-     * @return mixed
-     */
-    public function update($category, $data)
-    {
-        $category->update($data);
-
-        return $category;
-    }
-
-    /**
      * Return all categories in the given language
      * @param $lang
      * @return mixed
