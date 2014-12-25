fe1e15c503ad5f8de7857dd3db6debfc0905507d
Nicolas Widart <n.widart@gmail.com>
2014-12-25 10:34:57 +0100

    Remove unneeded method from concrete impl.

--- Repositories/Eloquent/EloquentCategoryRepository.php
+++ Repositories/Eloquent/EloquentCategoryRepository.php
@@ -6,16 +6,4 @@ use Modules\Core\Repositories\Eloquent\EloquentBaseRepository;
 
 class EloquentCategoryRepository extends EloquentBaseRepository implements CategoryRepository
 {
-    /**
-     * Return all categories in the given language
-     * @param $lang
-     * @return mixed
-     */
-    public function allTranslatedIn($lang)
-    {
-        return $this->model->whereHas('translations', function($q) use($lang)
-        {
-            $q->where('locale', "$lang");
-        })->get();
-    }
 }
