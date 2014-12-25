f0270a866ff41814cb8adeca848c0f70c2ed6662
Nicolas Widart <n.widart@gmail.com>
2014-12-25 14:09:55 +0100

    Remove unused class import

--- Repositories/Eloquent/EloquentCategoryRepository.php
+++ Repositories/Eloquent/EloquentCategoryRepository.php
@@ -1,6 +1,5 @@
 <?php namespace Modules\Blog\Repositories\Eloquent;
 
-use Modules\Blog\Entities\Category;
 use Modules\Blog\Repositories\CategoryRepository;
 use Modules\Core\Repositories\Eloquent\EloquentBaseRepository;
 
