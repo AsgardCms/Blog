b22058db96e46255eee40acafd00f854d6cb2238
Nicolas Widart <n.widart@gmail.com>
2014-12-25 14:11:49 +0100

    Type hint variable

--- Repositories/Eloquent/EloquentTagRepository.php
+++ Repositories/Eloquent/EloquentTagRepository.php
@@ -1,5 +1,6 @@
 <?php namespace Modules\Blog\Repositories\Eloquent;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\App;
 use Modules\Blog\Repositories\TagRepository;
 use Modules\Core\Repositories\Eloquent\EloquentBaseRepository;
@@ -13,7 +14,7 @@ class EloquentTagRepository extends EloquentBaseRepository implements TagReposit
      */
     public function findByName($name)
     {
-        $tags = $this->model->with('translations')->whereHas('translations', function($q) use($name)
+        $tags = $this->model->with('translations')->whereHas('translations', function(Builder $q) use($name)
         {
             $q->where('name', 'like', "%$name%");
         })->get();
