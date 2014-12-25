c369bcaed6990e7f9ac6756d1566071d9790a261
Nicolas Widart <n.widart@gmail.com>
2014-12-25 10:35:43 +0100

    Remove unneeded method from concrete impl.

--- Repositories/Cache/CacheCategoryDecorator.php
+++ Repositories/Cache/CacheCategoryDecorator.php
@@ -5,26 +5,4 @@ use Modules\Core\Repositories\Cache\BaseCacheDecorator;
 
 class CacheCategoryDecorator extends BaseCacheDecorator implements CategoryRepository
 {
-
-    /**
-     * Return resources translated in the given language
-     *
-     * @param $lang
-     * @return object
-     */
-    public function allTranslatedIn($lang)
-    {
-        // TODO: Implement allTranslatedIn() method.
-    }
-
-    /**
-     * Find a resource by the given slug
-     *
-     * @param int $slug
-     * @return object
-     */
-    public function findBySlug($slug)
-    {
-        // TODO: Implement findBySlug() method.
-    }
 }
