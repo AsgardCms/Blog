219a85d88f078431643f6c65e2f5a9093dc9634e
Nicolas Widart <n.widart@gmail.com>
2014-12-30 20:18:59 +0100

    Adding test to get only translated blog posts

---
+++ Tests/EloquentPostRepositoryTest.php
@@ -0,0 +1,27 @@
+<?php namespace Modules\Blog\Tests;
+
+class EloquentPostRepositoryTest extends BaseBlogTestCase
+{
+    /** @test */
+    public function it_should_only_returns_translated_posts()
+    {
+        // Prepare
+        $this->createBlogPost();
+        $this->createBlogPost();
+        $data = [
+            'en' => [
+                'title' => 'One language title',
+                'slug' => 'slug',
+                'content' => 'lorem ipsum'
+            ],
+            'category_id' => 1
+        ];
+        $this->post->create($data);
+
+        // Assert
+        $englishPosts = $this->post->allTranslatedIn('en');
+        $this->assertEquals(3, $englishPosts->count());
+        $frenchPosts = $this->post->allTranslatedIn('fr');
+        $this->assertEquals(2, $frenchPosts->count());
+    }
+}
