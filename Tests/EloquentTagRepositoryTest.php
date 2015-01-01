f367a5bb912a2cbcbd1e2d4b23094f9ac866e87d
Nicolas Widart <n.widart@gmail.com>
2015-01-01 10:52:17 +0100

    Test the find by tag name method

--- Tests/EloquentTagRepositoryTest.php
+++ Tests/EloquentTagRepositoryTest.php
@@ -2,5 +2,18 @@
 
 class EloquentTagRepositoryTest extends BaseBlogTestCase
 {
+    /** @test */
+    public function it_finds_a_tag_by_name()
+    {
+        // Prepare
+        $this->createTag();
+        $this->createTag();
+        $tag = $this->createTag();
 
+        // Run
+        $foundTag = $this->tag->findByName($tag->name);
+
+        // Assert
+        $this->assertEquals($tag->name, $foundTag[0]['name']);
+    }
 }
