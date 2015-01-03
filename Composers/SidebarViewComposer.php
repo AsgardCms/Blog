1a2cb6d917d2cf5eff770a4d96c2cb4ae6cf92fd
Nicolas Widart <n.widart@gmail.com>
2015-01-03 14:55:52 +0100

    Change permission name

--- Composers/SidebarViewComposer.php
+++ Composers/SidebarViewComposer.php
@@ -16,21 +16,21 @@ class SidebarViewComposer extends BaseSidebarViewComposer
                 'route' => '#',
                 'icon-class' => 'fa fa-copy',
                 'title' => 'Blog',
-                'permission' => $this->auth->hasAccess('posts.index') or $this->auth->hasAccess('categories.index'),
+                'permission' => $this->auth->hasAccess('blog.posts.index') or $this->auth->hasAccess('blog.categories.index'),
             ],
             [
                 'request' => "*/{$view->prefix}/blog/posts*",
                 'route' => 'admin.blog.post.index',
                 'icon-class' => 'fa fa-file-text',
                 'title' => 'Posts',
-                'permission' => $this->auth->hasAccess('posts.index')
+                'permission' => $this->auth->hasAccess('blog.posts.index')
             ],
             [
                 'request' => "*/{$view->prefix}/blog/categories*",
                 'route' => 'admin.blog.category.index',
                 'icon-class' => 'fa fa-file-text',
                 'title' => 'Categories',
-                'permission' => $this->auth->hasAccess('categories.index')
+                'permission' => $this->auth->hasAccess('blog.categories.index')
             ],
         ]));
     }
