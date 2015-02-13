24dfad993f543246b8d61a24bc241342f0d7bced
Nicolas Widart <n.widart@gmail.com>
2015-02-13 16:51:42 +0100

    Use new sidebar renderer

--- Composers/SidebarViewComposer.php
+++ Composers/SidebarViewComposer.php
@@ -1,37 +1,36 @@
 <?php namespace Modules\Blog\Composers;
 
 use Illuminate\Contracts\View\View;
-use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Request;
+use Maatwebsite\Sidebar\SidebarGroup;
+use Maatwebsite\Sidebar\SidebarItem;
 use Modules\Core\Composers\BaseSidebarViewComposer;
 
 class SidebarViewComposer extends BaseSidebarViewComposer
 {
     public function compose(View $view)
     {
-        $view->items->put('blog', Collection::make([
-            [
-                'weight' => '3',
-                'request' => Request::is("*/{$view->prefix}/blog/posts*") or Request::is("*/{$view->prefix}/blog/categories*"),
-                'route' => '#',
-                'icon-class' => 'fa fa-copy',
-                'title' => 'Blog',
-                'permission' => $this->auth->hasAccess('blog.posts.index') or $this->auth->hasAccess('blog.categories.index'),
-            ],
-            [
-                'request' => "*/{$view->prefix}/blog/posts*",
-                'route' => 'admin.blog.post.index',
-                'icon-class' => 'fa fa-file-text',
-                'title' => 'Posts',
-                'permission' => $this->auth->hasAccess('blog.posts.index')
-            ],
-            [
-                'request' => "*/{$view->prefix}/blog/categories*",
-                'route' => 'admin.blog.category.index',
-                'icon-class' => 'fa fa-file-text',
-                'title' => 'Categories',
-                'permission' => $this->auth->hasAccess('blog.categories.index')
-            ],
-        ]));
+        $view->sidebar->group('Blog', function (SidebarGroup $group) {
+            $group->authorize(
+                $this->auth->hasAccess('blog.posts.index') or $this->auth->hasAccess('blog.categories.index')
+            );
+
+            $group->addItem('posts', function (SidebarItem $item) {
+                $item->route('admin.user.user.index');
+                $item->icon = 'fa fa-copy';
+                $item->name = 'Blog';
+                $item->authorize(
+                    $this->auth->hasAccess('blog.posts.index')
+                );
+            });
+
+            $group->addItem('categories', function (SidebarItem $item) {
+                $item->route('admin.user.user.index');
+                $item->icon = 'fa fa-file-text';
+                $item->name = 'Categories';
+                $item->authorize(
+                    $this->auth->hasAccess('blog.categories.index')
+                );
+            });
+        });
     }
 }
