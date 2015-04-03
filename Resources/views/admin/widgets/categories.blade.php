8ea7b576544eb281e49767b663ae4109ef622e00
Nicolas Widart <n.widart@gmail.com>
2015-04-03 20:45:31 +0200

    Wrapping widgets

--- Resources/views/admin/widgets/categories.blade.php
+++ Resources/views/admin/widgets/categories.blade.php
@@ -1,15 +1,17 @@
-<div class="info-box bg-aqua">
-    <span class="info-box-icon"><i class="fa fa-bookmark-o"></i></span>
-    <div class="info-box-content">
-        <span class="info-box-text">Categories</span>
-        <span class="info-box-number">{{ $categoryCount }}</span>
+<div class="col-md-12">
+    <div class="info-box bg-aqua">
+        <span class="info-box-icon"><i class="fa fa-bookmark-o"></i></span>
+        <div class="info-box-content">
+            <span class="info-box-text">Categories</span>
+            <span class="info-box-number">{{ $categoryCount }}</span>
 
-        <div class="progress">
-            <div class="progress-bar" style="width: 70%"></div>
+            <div class="progress">
+                <div class="progress-bar" style="width: 70%"></div>
+            </div>
+                      <span class="progress-description">
+                        70% Increase in 30 Days
+                      </span>
         </div>
-                  <span class="progress-description">
-                    70% Increase in 30 Days
-                  </span>
+        <!-- /.info-box-content -->
     </div>
-    <!-- /.info-box-content -->
 </div>
