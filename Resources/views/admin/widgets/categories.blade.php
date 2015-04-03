3fb5e8d3eb06bcbb7afe611388bbea96e7259bfc
Nicolas Widart <n.widart@gmail.com>
2015-04-03 20:48:50 +0200

    Using padding to separate widgets

--- Resources/views/admin/widgets/categories.blade.php
+++ Resources/views/admin/widgets/categories.blade.php
@@ -1,17 +1,15 @@
-<div class="col-md-12">
-    <div class="info-box bg-aqua">
-        <span class="info-box-icon"><i class="fa fa-bookmark-o"></i></span>
-        <div class="info-box-content">
-            <span class="info-box-text">Categories</span>
-            <span class="info-box-number">{{ $categoryCount }}</span>
+<div class="info-box bg-aqua">
+    <span class="info-box-icon"><i class="fa fa-bookmark-o"></i></span>
+    <div class="info-box-content">
+        <span class="info-box-text">Categories</span>
+        <span class="info-box-number">{{ $categoryCount }}</span>
 
-            <div class="progress">
-                <div class="progress-bar" style="width: 70%"></div>
-            </div>
-                      <span class="progress-description">
-                        70% Increase in 30 Days
-                      </span>
+        <div class="progress">
+            <div class="progress-bar" style="width: 70%"></div>
         </div>
-        <!-- /.info-box-content -->
+                  <span class="progress-description">
+                    70% Increase in 30 Days
+                  </span>
     </div>
+    <!-- /.info-box-content -->
 </div>
--- Resources/views/admin/widgets/posts.blade.php
+++ Resources/views/admin/widgets/posts.blade.php
@@ -1,19 +1,17 @@
-<div class="col-md-12">
-    <div class="info-box bg-aqua">
-        <span class="info-box-icon"><i class="fa fa-bookmark-o"></i></span>
-        <div class="info-box-content">
-        <span class="info-box-text">
-            PostCount
-        </span>
-            <span class="info-box-number">{{ $postCount }}</span>
+<div class="info-box bg-aqua">
+    <span class="info-box-icon"><i class="fa fa-bookmark-o"></i></span>
+    <div class="info-box-content">
+    <span class="info-box-text">
+        PostCount
+    </span>
+        <span class="info-box-number">{{ $postCount }}</span>
 
-            <div class="progress">
-                <div class="progress-bar" style="width: 70%"></div>
-            </div>
-                  <span class="progress-description">
-                    70% Increase in 30 Days
-                  </span>
+        <div class="progress">
+            <div class="progress-bar" style="width: 70%"></div>
         </div>
-        <!-- /.info-box-content -->
+              <span class="progress-description">
+                70% Increase in 30 Days
+              </span>
     </div>
+    <!-- /.info-box-content -->
 </div>
