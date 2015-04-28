11b1db9ce28229fe2f1b04e21e7d762e121f7bd9
Nicolas Widart <n.widart@gmail.com>
2015-04-28 20:52:40 +0200

    Adding has translations check

--- Resources/views/admin/categories/partials/edit-fields.blade.php
+++ Resources/views/admin/categories/partials/edit-fields.blade.php
@@ -1,12 +1,14 @@
 <div class="box-body">
     <div class='form-group{{ $errors->has("{$lang}[name]") ? ' has-error' : '' }}'>
         {!! Form::label("{$lang}[name]", trans('blog::category.form.name')) !!}
-        {!! Form::text("{$lang}[name]", Input::old("{$lang}[name]", $category->translate($lang)->name), ['class' => 'form-control slugify', 'placeholder' => trans('blog::category.form.name')]) !!}
+        <?php $old = $category->hasTranslation($locale) ? $category->translate($lang)->name : '' ?>
+        {!! Form::text("{$lang}[name]", Input::old("{$lang}[name]", $old), ['class' => 'form-control slugify', 'placeholder' => trans('blog::category.form.name')]) !!}
         {!! $errors->first("{$lang}[name]", '<span class="help-block">:message</span>') !!}
     </div>
     <div class='form-group{{ $errors->has("{$lang}[slug]") ? ' has-error' : '' }}'>
            {!! Form::label("{$lang}[slug]", trans('blog::category.form.slug')) !!}
-           {!! Form::text("{$lang}[slug]", Input::old("{$lang}[slug]", $category->translate($lang)->slug), ['class' => 'form-control slug', 'placeholder' => trans('blog::category.form.slug')]) !!}
+            <?php $old = $category->hasTranslation($locale) ? $category->translate($lang)->slug : '' ?>
+           {!! Form::text("{$lang}[slug]", Input::old("{$lang}[slug]", $old), ['class' => 'form-control slug', 'placeholder' => trans('blog::category.form.slug')]) !!}
            {!! $errors->first("{$lang}[slug]", '<span class="help-block">:message</span>') !!}
        </div>
 </div>
