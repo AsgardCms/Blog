b079e634c56c3e88104e0a5edf4559a289778ce0
Nicolas Widart <n.widart@gmail.com>
2015-05-26 19:49:27 +0200

    Get the latest posts amount from config

--- Presenters/PostPresenter.php
+++ Presenters/PostPresenter.php
@@ -15,8 +15,21 @@ class PostPresenter extends Presenter
         $this->post = app('Modules\Blog\Repositories\PostRepository');
     }
 
+    /**
+     * Get the previous post of the current post
+     * @return object
+     */
     public function previous()
     {
         return $this->post->getPreviousOf($this->entity);
+    }
+
+    /**
+     * Get the next post of the current post
+     * @return object
+     */
+    public function next()
+    {
+        return $this->post->getNextOf($this->entity);
     }
 }
