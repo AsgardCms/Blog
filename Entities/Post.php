f8c99ae2f225daa77976aa391f6c9eafa4a75063
Nicolas Widart <n.widart@gmail.com>
2015-05-29 20:11:31 +0200

    Add the status as fillable attribute

--- Entities/Post.php
+++ Entities/Post.php
@@ -10,7 +10,7 @@ class Post extends Model
     use Translatable, MediaRelation, PresentableTrait;
 
     public $translatedAttributes = ['title', 'slug', 'content'];
-    protected $fillable = ['category_id', 'title', 'slug', 'content'];
+    protected $fillable = ['category_id', 'status', 'title', 'slug', 'content'];
     protected $table = 'blog__posts';
     protected $presenter = 'Modules\Blog\Presenters\PostPresenter';
 
