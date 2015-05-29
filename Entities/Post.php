8758669ae83ddde83adf2d6f022b00c770af7760
Nicolas Widart <n.widart@gmail.com>
2015-05-29 21:01:59 +0200

    Adding query scopes for post statuses

--- Entities/Post.php
+++ Entities/Post.php
@@ -1,6 +1,7 @@
 <?php namespace Modules\Blog\Entities;
 
 use Dimsav\Translatable\Translatable;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Laracasts\Presenter\PresentableTrait;
 use Modules\Media\Support\Traits\MediaRelation;
@@ -25,5 +26,45 @@ class Post extends Model
     public function tags()
     {
         return $this->belongsToMany('Modules\Blog\Entities\Tag', 'blog__post_tag');
+    }
+
+    /**
+     * Check if the post is in draft
+     * @param Builder $query
+     * @return bool
+     */
+    public function scopeDraft(Builder $query)
+    {
+        return (bool) $query->whereStatus(0);
+    }
+
+    /**
+     * Check if the post is pending review
+     * @param Builder $query
+     * @return bool
+     */
+    public function scopePending(Builder $query)
+    {
+        return (bool) $query->whereStatus(1);
+    }
+
+    /**
+     * Check if the post is published
+     * @param Builder $query
+     * @return bool
+     */
+    public function scopePublished(Builder $query)
+    {
+        return (bool) $query->whereStatus(2);
+    }
+
+    /**
+     * Check if the post is unpublish
+     * @param Builder $query
+     * @return bool
+     */
+    public function scopeUnpublished(Builder $query)
+    {
+        return (bool) $query->whereStatus(3);
     }
 }
