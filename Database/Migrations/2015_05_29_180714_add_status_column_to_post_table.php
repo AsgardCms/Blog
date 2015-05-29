eba52f3d19b8b37dea3b508332704232a71fa876
Nicolas Widart <n.widart@gmail.com>
2015-05-29 20:10:12 +0200

    Adding migration to add status table to posts table

---
+++ Database/Migrations/2015_05_29_180714_add_status_column_to_post_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddStatusColumnToPostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('blog__posts', function (Blueprint $table) {
+            $table->integer('status')->after('category_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('blog__posts', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+    }
+
+}
