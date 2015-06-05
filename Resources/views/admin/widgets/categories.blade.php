27585060efd636a61ec13fcab24e69b30adaac91
Nicolas Widart <n.widart@gmail.com>
2015-06-05 19:58:29 +0200

    Remove unneeded data

--- Resources/views/admin/widgets/categories.blade.php
+++ Resources/views/admin/widgets/categories.blade.php
@@ -3,13 +3,6 @@
     <div class="info-box-content">
         <span class="info-box-text">Categories</span>
         <span class="info-box-number">{{ $categoryCount }}</span>
-
-        <div class="progress">
-            <div class="progress-bar" style="width: 70%"></div>
-        </div>
-                  <span class="progress-description">
-                    70% Increase in 30 Days
-                  </span>
     </div>
     <!-- /.info-box-content -->
 </div>
--- Resources/views/admin/widgets/posts.blade.php
+++ Resources/views/admin/widgets/posts.blade.php
@@ -5,13 +5,6 @@
         PostCount
     </span>
         <span class="info-box-number">{{ $postCount }}</span>
-
-        <div class="progress">
-            <div class="progress-bar" style="width: 70%"></div>
-        </div>
-              <span class="progress-description">
-                70% Increase in 30 Days
-              </span>
     </div>
     <!-- /.info-box-content -->
 </div>
