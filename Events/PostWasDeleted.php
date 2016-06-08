d8df5a135042681082187cdbce5eb6442672e47b
Nicolas Widart <n.widart@gmail.com>
2016-06-08 21:35:57 +0200

    Create a postDeleted event

---
+++ Events/PostWasDeleted.php
@@ -0,0 +1,39 @@
+<?php namespace Modules\Blog\Events;
+
+use Modules\Media\Contracts\DeletingMedia;
+
+class PostWasDeleted implements DeletingMedia
+{
+    /**
+     * @var string
+     */
+    private $postClass;
+    /**
+     * @var int
+     */
+    private $postId;
+
+    public function __construct($postId, $postClass)
+    {
+        $this->postClass = $postClass;
+        $this->postId = $postId;
+    }
+
+    /**
+     * Get the entity ID
+     * @return int
+     */
+    public function getEntityId()
+    {
+        return $this->postId;
+    }
+
+    /**
+     * Get the class name the imageables
+     * @return string
+     */
+    public function getClassName()
+    {
+        return $this->postClass;
+    }
+}
