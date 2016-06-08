95d0a53b23f6d245492aaefd396c3a75f730fe63
Nicolas Widart <n.widart@gmail.com>
2016-06-08 21:26:45 +0200

    Using $router variable

--- Http/apiRoutes.php
+++ Http/apiRoutes.php
@@ -1,4 +1,8 @@
 <?php
 
+use Illuminate\Routing\Router;
+
+/** @var Router $router */
+
 $router->resource('tag', 'TagController');
 $router->get('tag/findByName/{name?}', ['as' => 'api.tag.findByName', 'uses' => 'TagController@findByName']);
--- Http/backendRoutes.php
+++ Http/backendRoutes.php
@@ -1,5 +1,9 @@
 <?php
 
+use Illuminate\Routing\Router;
+
+/** @var Router $router */
+
 $router->bind('category', function ($id) {
     return app(\Modules\Blog\Repositories\CategoryRepository::class)->find($id);
 });
@@ -11,26 +15,26 @@ $router->bind('tag', function ($id) {
     return app(Modules\Blog\Repositories\TagRepository::class)->find($id);
 });
 
-$router->group(['prefix' => '/blog'], function () {
-    get('posts', ['as' => 'admin.blog.post.index', 'uses' => 'PostController@index']);
-    get('posts/create', ['as' => 'admin.blog.post.create', 'uses' => 'PostController@create']);
-    post('posts', ['as' => 'admin.blog.post.store', 'uses' => 'PostController@store']);
-    get('posts/{post}/edit', ['as' => 'admin.blog.post.edit', 'uses' => 'PostController@edit']);
-    put('posts/{post}', ['as' => 'admin.blog.post.update', 'uses' => 'PostController@update']);
-    delete('posts/{post}', ['as' => 'admin.blog.post.destroy', 'uses' => 'PostController@destroy']);
+$router->group(['prefix' => '/blog'], function (Router $router) {
+    $router->get('posts', ['as' => 'admin.blog.post.index', 'uses' => 'PostController@index']);
+    $router->get('posts/create', ['as' => 'admin.blog.post.create', 'uses' => 'PostController@create']);
+    $router->post('posts', ['as' => 'admin.blog.post.store', 'uses' => 'PostController@store']);
+    $router->get('posts/{post}/edit', ['as' => 'admin.blog.post.edit', 'uses' => 'PostController@edit']);
+    $router->put('posts/{post}', ['as' => 'admin.blog.post.update', 'uses' => 'PostController@update']);
+    $router->delete('posts/{post}', ['as' => 'admin.blog.post.destroy', 'uses' => 'PostController@destroy']);
 
-    get('categories', ['as' => 'admin.blog.category.index', 'uses' => 'CategoryController@index']);
-    get('categories/create', ['as' => 'admin.blog.category.create', 'uses' => 'CategoryController@create']);
-    post('categories', ['as' => 'admin.blog.category.store', 'uses' => 'CategoryController@store']);
-    get('categories/{category}/edit', ['as' => 'admin.blog.category.edit', 'uses' => 'CategoryController@edit']);
-    put('categories/{category}', ['as' => 'admin.blog.category.update', 'uses' => 'CategoryController@update']);
-    delete('categories/{category}', ['as' => 'admin.blog.category.destroy', 'uses' => 'CategoryController@destroy']);
+    $router->get('categories', ['as' => 'admin.blog.category.index', 'uses' => 'CategoryController@index']);
+    $router->get('categories/create', ['as' => 'admin.blog.category.create', 'uses' => 'CategoryController@create']);
+    $router->post('categories', ['as' => 'admin.blog.category.store', 'uses' => 'CategoryController@store']);
+    $router->get('categories/{category}/edit', ['as' => 'admin.blog.category.edit', 'uses' => 'CategoryController@edit']);
+    $router->put('categories/{category}', ['as' => 'admin.blog.category.update', 'uses' => 'CategoryController@update']);
+    $router->delete('categories/{category}', ['as' => 'admin.blog.category.destroy', 'uses' => 'CategoryController@destroy']);
 
-    get('tags', ['as' => 'admin.blog.tag.index', 'uses' => 'TagController@index']);
-    get('tags/create', ['as' => 'admin.blog.tag.create', 'uses' => 'TagController@create']);
-    post('tags', ['as' => 'admin.blog.tag.store', 'uses' => 'TagController@store']);
-    get('tags/{tag}/edit', ['as' => 'admin.blog.tag.edit', 'uses' => 'TagController@edit']);
-    put('tags/{tag}', ['as' => 'admin.blog.tag.update', 'uses' => 'TagController@update']);
-    delete('tags/{tag}', ['as' => 'admin.blog.tag.destroy', 'uses' => 'TagController@destroy']);
+    $router->get('tags', ['as' => 'admin.blog.tag.index', 'uses' => 'TagController@index']);
+    $router->get('tags/create', ['as' => 'admin.blog.tag.create', 'uses' => 'TagController@create']);
+    $router->post('tags', ['as' => 'admin.blog.tag.store', 'uses' => 'TagController@store']);
+    $router->get('tags/{tag}/edit', ['as' => 'admin.blog.tag.edit', 'uses' => 'TagController@edit']);
+    $router->put('tags/{tag}', ['as' => 'admin.blog.tag.update', 'uses' => 'TagController@update']);
+    $router->delete('tags/{tag}', ['as' => 'admin.blog.tag.destroy', 'uses' => 'TagController@destroy']);
 
 });
