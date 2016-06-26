8ad3fa16799540a4f2ded6c7714e0ef16575c99f
Nicolas Widart <n.widart@gmail.com>
2016-06-26 20:50:24 +0200

    Adding the current user to blog posts views

--- composers.php
+++ composers.php
@@ -4,3 +4,8 @@ view()->composer(
     config('asgard.blog.config.latest-posts', ['blog.*']),
     \Modules\Blog\Composers\Frontend\LatestPostsComposer::class
 );
+
+view()->composer([
+    'blog::admin.posts.create',
+    'blog::admin.posts.edit',
+], \Modules\Core\Composers\CurrentUserViewComposer::class);
