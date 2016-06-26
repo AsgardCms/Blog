5003d12b8dd3549599039a6b48f8b3ca21ecd10f
Nicolas Widart <n.widart@gmail.com>
2016-06-26 20:51:55 +0200

    Protecting the tag api URIs

--- Http/apiRoutes.php
+++ Http/apiRoutes.php
@@ -3,12 +3,13 @@
 use Illuminate\Routing\Router;
 
 /** @var Router $router */
-
-$router->post('tag', [
-    'as' => 'api.tag.store',
-    'uses' => 'TagController@store'
-]);
-$router->get('tag/findByName/{name?}', [
-    'as' => 'api.tag.findByName',
-    'uses' => 'TagController@findByName',
-]);
+$router->group(['prefix' => 'v1', 'middleware' => 'api.token.admin'], function (Router $router) {
+    $router->post('tag', [
+        'as' => 'api.tag.store',
+        'uses' => 'TagController@store'
+    ]);
+    $router->get('tag/findByName/{name?}', [
+        'as' => 'api.tag.findByName',
+        'uses' => 'TagController@findByName',
+    ]);
+});
