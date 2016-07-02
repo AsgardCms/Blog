adf89f4e8dbe8f126f82fd1878d7743d4be05c23
Nicolas Widart <n.widart@gmail.com>
2016-07-02 14:24:56 +0200

    Adding api endpoints to handle the blog categories

---
+++ Http/Controllers/Api/V1/CategoryController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Modules\Blog\Http\Controllers\Api\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Routing\Controller;
+use Modules\Blog\Entities\Category;
+use Modules\Blog\Http\Requests\StoreCategoryRequest;
+use Modules\Blog\Http\Requests\UpdateCategoryRequest;
+use Modules\Blog\Repositories\CategoryRepository;
+
+class CategoryController extends Controller
+{
+    /**
+     * @var CategoryRepository
+     */
+    private $category;
+
+    public function __construct(CategoryRepository $category)
+    {
+        $this->category = $category;
+    }
+
+    public function index()
+    {
+        $categories = $this->category->all();
+
+        return response()->json([
+            'errors' => false,
+            'count' => $categories->count(),
+            'data' => $categories,
+        ]);
+    }
+
+    public function store(StoreCategoryRequest $request)
+    {
+        $category = $this->category->create($request->all());
+
+        return response()->json([
+            'errors' => false,
+            'message' => trans('blog::messages.category created'),
+            'data' => $category,
+        ], Response::HTTP_CREATED);
+    }
+
+    public function update(Category $category, Request $request)
+    {
+        $category = $this->category->update($category, $request->all());
+
+        return response()->json([
+            'errors' => false,
+            'message' => trans('blog::messages.category updated'),
+            'data' => $category,
+        ], Response::HTTP_CREATED);
+    }
+
+    public function destroy(Category $category)
+    {
+        $this->category->destroy($category);
+
+        return response()->json([
+            'errors' => false,
+            'message' => trans('blog::messages.category deleted'),
+        ], Response::HTTP_ACCEPTED);
+    }
+}
--- Http/apiRoutes.php
+++ Http/apiRoutes.php
@@ -13,3 +13,29 @@ $router->group(['prefix' => 'v1', 'middleware' => 'api.token.admin'], function (
         'uses' => 'TagController@findByName',
     ]);
 });
+
+$router->group(['prefix' => 'v1/blog', 'middleware' => 'api.token.admin'], function (Router $router) {
+    $router->get('categories', [
+        'as' => 'api.blog.category.index',
+        'uses' => 'V1\CategoryController@index',
+        'middleware' => 'token-can:blog.categories.index',
+    ]);
+
+    $router->post('categories', [
+        'as' => 'api.blog.category.store',
+        'uses' => 'V1\CategoryController@store',
+        'middleware' => 'token-can:blog.categories.create',
+    ]);
+
+    $router->post('categories/{category}', [
+        'as' => 'api.blog.category.update',
+        'uses' => 'V1\CategoryController@update',
+        'middleware' => 'token-can:blog.categories.edit',
+    ]);
+
+    $router->delete('categories/{category}', [
+        'as' => 'api.blog.category.destroy',
+        'uses' => 'V1\CategoryController@destroy',
+        'middleware' => 'token-can:blog.categories.destroy',
+    ]);
+});
