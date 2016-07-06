66bef81636538ce723741d2f98a0738350d56dc2
Nicolas Widart <n.widart@gmail.com>
2016-07-06 21:02:19 +0200

    Using named permissions

--- Config/permissions.php
+++ Config/permissions.php
@@ -2,21 +2,21 @@
 
 return [
     'blog.posts' => [
-        'index',
-        'create',
-        'edit',
-        'destroy',
+        'index' => trans('blog::post.list resource'),
+        'create' => trans('blog::post.create resource'),
+        'edit' => trans('blog::post.edit resource'),
+        'destroy' => trans('blog::post.destroy resource'),
     ],
     'blog.categories' => [
-        'index',
-        'create',
-        'edit',
-        'destroy',
+        'index' => trans('blog::category.list resource'),
+        'create' => trans('blog::category.create resource'),
+        'edit' => trans('blog::category.edit resource'),
+        'destroy' => trans('blog::category.destroy resource'),
     ],
     'blog.tags' => [
-        'index',
-        'create',
-        'edit',
-        'destroy',
+        'index' => trans('blog::tag.list resource'),
+        'create' => trans('blog::tag.create resource'),
+        'edit' => trans('blog::tag.edit resource'),
+        'destroy' => trans('blog::tag.destroy resource'),
     ],
 ];
