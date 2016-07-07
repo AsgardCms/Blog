6aff85970c069f08973f13776a1c67ac5f9d4de0
Nicolas Widart <n.widart@gmail.com>
2016-07-07 20:45:47 +0200

    Only use the translations keys in config files, and translate in the view

--- Config/permissions.php
+++ Config/permissions.php
@@ -2,21 +2,21 @@
 
 return [
     'blog.posts' => [
-        'index' => trans('blog::post.list resource'),
-        'create' => trans('blog::post.create resource'),
-        'edit' => trans('blog::post.edit resource'),
-        'destroy' => trans('blog::post.destroy resource'),
+        'index' => 'blog::post.list resource',
+        'create' => 'blog::post.create resource',
+        'edit' => 'blog::post.edit resource',
+        'destroy' => 'blog::post.destroy resource',
     ],
     'blog.categories' => [
-        'index' => trans('blog::category.list resource'),
-        'create' => trans('blog::category.create resource'),
-        'edit' => trans('blog::category.edit resource'),
-        'destroy' => trans('blog::category.destroy resource'),
+        'index' => 'blog::category.list resource',
+        'create' => 'blog::category.create resource',
+        'edit' => 'blog::category.edit resource',
+        'destroy' => 'blog::category.destroy resource',
     ],
     'blog.tags' => [
-        'index' => trans('blog::tag.list resource'),
-        'create' => trans('blog::tag.create resource'),
-        'edit' => trans('blog::tag.edit resource'),
-        'destroy' => trans('blog::tag.destroy resource'),
+        'index' => 'blog::tag.list resource',
+        'create' => 'blog::tag.create resource',
+        'edit' => 'blog::tag.edit resource',
+        'destroy' => 'blog::tag.destroy resource',
     ],
 ];
