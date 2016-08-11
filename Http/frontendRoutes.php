0a5f4a1d4d31ab53012fbf8ca78a55b763b4f342
Foulques du Peloux <foulques@dupeloux.fr>
2016-08-11 13:54:26 +0200

    Add middleware support
    As in the Page module, this code adds support to middlewares.

--- Http/frontendRoutes.php
+++ Http/frontendRoutes.php
@@ -6,6 +6,14 @@ use Illuminate\Routing\Router;
 
 $router->group(['prefix' => 'blog'], function (Router $router) {
     $locale = LaravelLocalization::setLocale() ?: App::getLocale();
-    $router->get('posts', ['as' => $locale . '.blog', 'uses' => 'PublicController@index']);
-    $router->get('posts/{slug}', ['as' => $locale . '.blog.slug', 'uses' => 'PublicController@show']);
+    $router->get('posts', [
+        'as' => $locale . '.blog',
+        'uses' => 'PublicController@index',
+        'middleware' => config('asgard.blog.config.middleware')
+    ]);
+    $router->get('posts/{slug}', [
+        'as' => $locale . '.blog.slug',
+        'uses' => 'PublicController@show',
+        'middleware' => config('asgard.blog.config.middleware')
+    ]);
 });
