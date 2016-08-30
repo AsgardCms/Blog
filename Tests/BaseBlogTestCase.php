3074038cfb18c67fde83f9cb5f75edfe1a152335
Nicolas Widart <n.widart@gmail.com>
2016-08-30 17:34:01 +0200

    Preparing tests

--- Tests/BaseBlogTestCase.php
+++ Tests/BaseBlogTestCase.php
@@ -4,9 +4,15 @@ namespace Modules\Blog\Tests;
 
 use Faker\Factory;
 use Illuminate\Support\Str;
-use Modules\Core\Tests\BaseTestCase;
+use Maatwebsite\Sidebar\SidebarServiceProvider;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+use Mcamara\LaravelLocalization\LaravelLocalizationServiceProvider;
+use Modules\Blog\Providers\BlogServiceProvider;
+use Modules\Core\Providers\CoreServiceProvider;
+use Nwidart\Modules\LaravelModulesServiceProvider;
+use Orchestra\Testbench\TestCase;
 
-abstract class BaseBlogTestCase extends BaseTestCase
+abstract class BaseBlogTestCase extends TestCase
 {
     /**
      * @var \Modules\Blog\Repositories\PostRepository
@@ -22,12 +28,40 @@ abstract class BaseBlogTestCase extends BaseTestCase
     {
         parent::setUp();
 
-        /** @var \Illuminate\Console\Application $artisan */
-        $artisan = $this->app->make('Illuminate\Console\Application');
-        $artisan->call('module:migrate', ['module' => 'Blog']);
+        $this->resetDatabase();
 
         $this->post = app('Modules\Blog\Repositories\PostRepository');
         $this->tag = app('Modules\Blog\Repositories\TagRepository');
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            LaravelModulesServiceProvider::class,
+            LaravelLocalizationServiceProvider::class,
+            CoreServiceProvider::class,
+            BlogServiceProvider::class,
+            SidebarServiceProvider::class,
+        ];
+    }
+
+    protected function getPackageAliases($app)
+    {
+        return [
+            'LaravelLocalization' => LaravelLocalization::class,
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['path.base'] = __DIR__ . '/..';
+        $app['config']->set('database.default', 'sqlite');
+        $app['config']->set('database.connections.sqlite', array(
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ));
+        $app['config']->set('translatable.locales', ['en', 'fr']);
     }
 
     /**
@@ -80,5 +114,25 @@ abstract class BaseBlogTestCase extends BaseTestCase
         ];
 
         return $this->tag->create($data);
+    }
+
+    private function resetDatabase()
+    {
+        // Relative to the testbench app folder: vendors/orchestra/testbench/src/fixture
+        $migrationsPath = realpath('Database/Migrations');
+        // Makes sure the migrations table is created
+        $this->artisan('migrate', [
+            '--database' => 'sqlite',
+            '--realpath' => $migrationsPath,
+        ]);
+        // We empty all tables
+        $this->artisan('migrate:reset', [
+            '--database' => 'sqlite',
+        ]);
+        // Migrate
+        $this->artisan('migrate', [
+            '--database' => 'sqlite',
+            '--realpath'     => $migrationsPath,
+        ]);
     }
 }
