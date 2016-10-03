2c63ec0a19222e6da6686f3216a424ea45ddd423
Micheal Mand <micheal@kmdwebdesigns.com>
2016-10-03 12:12:17 -0600

    Add another option to control the number of latest posts shown on the dashboard
    If the user wishes to show many latest posts on their site (10 or more,
    for example), the widget grows uncontrollably big; especially if the
    titles are fairly long - they end up wrapping and making the widget even
    longer.
    
    Signed-off-by: Micheal Mand <micheal@kmdwebdesigns.com>

--- Resources/lang/en/settings.php
+++ Resources/lang/en/settings.php
@@ -3,4 +3,5 @@
 return [
     'posts-per-page' => 'Posts per page',
     'latest-posts-amount' => 'Amount of latest posts',
+    'widget-posts-amount' => 'Amount of latest posts to show on the dashboard widget',
 ];
