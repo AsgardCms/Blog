fe6a4d728fb174868fab425ba82ed033e1e327fc
Nicolas Widart <n.widart@gmail.com>
2017-06-15 16:20:12 +0200

    Updating method name getParameter to parameter() for laravel 5.4

--- Http/Requests/UpdatePostRequest.php
+++ Http/Requests/UpdatePostRequest.php
@@ -13,7 +13,7 @@ class UpdatePostRequest extends BaseFormRequest
 
     public function translationRules()
     {
-        $id = $this->route()->getParameter('post')->id;
+        $id = $this->route()->parameter('post')->id;
 
         return [
             "title" => "required",
